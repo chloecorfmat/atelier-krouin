c1684e4525393146e5d12ba1d06a8972eee1d59d
Luuka <luuka.tr@gmail.com>
2021-10-17 21:17:59 +0200

    Rm nuxtDeploy

--- apache/nuxtDeploy/_nuxt/static/1634474374/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Farticles","\u002Fcategories\u002Ftest2","\u002Farticle\u002Ftest","\u002Farticles\u002F2","\u002Farticles\u002F1"]})
--- apache/nuxtDeploy/_nuxt/static/1634475961/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Farticle\u002Ftest","\u002Farticle\u002Fencore-un-test","\u002Farticles","\u002Fcategories\u002Ftest2","\u002F","\u002Farticles\u002F2","\u002Farticles\u002F1"]})
