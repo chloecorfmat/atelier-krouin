97359c7a3917be41f581283d850330bd7a47ae3f
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2021-10-31 21:51:15 +0100

    Update introduction

--- nuxt/components/BlockIntroducing.vue
+++ nuxt/components/BlockIntroducing.vue
@@ -3,7 +3,7 @@ a<template>
   <div class="block block--introducing">
     <img class="introducing--image" src="/images/photo_profil.jpg" alt="Introducing" />
     <div class="introducing--infos">
-      <p class="introducing--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Venenatis scelerisque at quam congue posuere libero in sit quam. Consequat, scelerisque non tincidunt sit lectus senectus. Lorem ipsum dolor sit amet, libero in sit quam.</p>
+      <p class="introducing--text">Ingénieure dans le domaine du web, je passe la plupart de mon temps libre à créer : couture, peinture, perles, broderie, DIY, scrapbooking et bien plus encore. J'aime toucher à tout et découvrir de nouvelles activités. Découvrez ici mes réalisations, des tutoriels et échangeons ensemble via Instagram.</p>
       <p class="introducing--signature">Chloé</p>
     </div>
   </div>
