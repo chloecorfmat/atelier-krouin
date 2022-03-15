7f205057b68412b0b905796304f2cd0591267a9c
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2022-03-15 12:12:00 +0100

    Remove meta charset

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -21,7 +21,6 @@ export default {
       lang: 'fr'
     },
     meta: [
-      { charset: 'utf-8' },
       { 'http-equiv':"Content-Type", content: 'text/html; charset=utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'L\'espace créatif de Chloé : un peu de peinture, beaucoup de couture, énormément de DIY, toujours plus de passion et de partage.' },
